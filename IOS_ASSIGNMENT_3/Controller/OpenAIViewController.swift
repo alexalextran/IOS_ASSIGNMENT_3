eb8b2a5af0b3cc78b92c27b581afbc0d45d23612
Alex Tran <alextran8385@gmail.com>
2023-05-19 23:20:03 +1000

    Created all custom view controllers and added some labels

---
+++ IOS_ASSIGNMENT_3/Controller/OpenAIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IOS_ASSIGNMENT_3
+//
+//  Created by Alex Tran on 18/5/2023.
+//
+
+import UIKit
+
+class OpenAIViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    var nice:Int = 10;
+ 
+
+
+}
+
---
+++ IOS_ASSIGNMENT_3/Controller/StatisticsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IOS_ASSIGNMENT_3
+//
+//  Created by Alex Tran on 18/5/2023.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    var nice:Int = 10;
+ 
+
+
+}
+
