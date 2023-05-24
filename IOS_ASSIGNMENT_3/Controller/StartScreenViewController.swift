a5fbe0941b604b577915f5e51f0f0b85b8368fba
Joshua Nguyen <joshua.nguyen07@gmail.com>
2023-05-24 18:53:08 +1000

    added code to stop rotation and to stay in portrait mode the entire time

--- IOS_ASSIGNMENT_3/Controller/StartScreenViewController.swift
+++ IOS_ASSIGNMENT_3/Controller/StartScreenViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class StartScreenViewController: UIViewController {
-    
+    /*override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait // Set the desired orientation to portrait only
+    }*/
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
  
+    override var shouldAutorotate: Bool {
+        return false // Disable auto rotation
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait // Set the desired orientation (e.g., portrait)
+    }
 
 
 }
