92f7ad9f6f8a9ce67f1438e07435a281445a0762
Joshua Nguyen <joshua.nguyen07@gmail.com>
2023-05-24 21:38:57 +1000

    added code to change the current month of the recap page with arrow buttons and it shows their respective month's days... still working on it though

--- IOS_ASSIGNMENT_3/Controller/EntryRecapViewController.swift
+++ IOS_ASSIGNMENT_3/Controller/EntryRecapViewController.swift
@@ -1,25 +1,102 @@
 import UIKit
 
 class EntryRecapViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var NextMonth: UIButton!
+    @IBOutlet weak var PreviousMonth: UIButton!
+    @IBOutlet weak var CurrentMonthandYear: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
+    let monthsDictionary =
+    [1: 31,
+     2: 28,
+     3: 31,
+     4: 30,
+     5: 31,
+     6: 30,
+     7: 31,
+     8: 31,
+     9: 30,
+     10: 31,
+     11: 30,
+     12: 31]
     
+    var i:Int = 0
     var selectedDate: Date?
     var formattedDate: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateCurrentMonthAndYearLabel()
+        
+        PreviousMonth.addTarget(self, action: #selector(previousMonthButtonTapped(_:)), for: .touchUpInside)
+        
+        NextMonth.addTarget(self, action: #selector(nextMonthButtonTapped(_:)), for: .touchUpInside)
+
         // Set up the collection view
         collectionView.delegate = self
         collectionView.dataSource = self
         
         // Register the cell class for the collection view
         collectionView.register(DateCell.self, forCellWithReuseIdentifier: "DateCell")
+        
+        let currentDate = Date()
+        let calendar = Calendar.current
+
+        let currentMonth = calendar.component(.month, from: currentDate)
+        print("Current month (digit): \(currentMonth)")
+        
+        i = currentMonth
+        
     }
+    func updateCurrentMonthAndYearLabel() {
+        let monthName = DateFormatter().monthSymbols[i - 1]
+        let currentYear = Calendar.current.component(.year, from: Date())
+        CurrentMonthandYear.text = "\(monthName) \(currentYear)"
+    }
+    
+    //decreases the value of i by 1 and therefore moving to the previous month
+    @objc func previousMonthButtonTapped(_ sender: UIButton) {
+            // Decrease the value of i by 1
+            i -= 1
+            
+            // Ensure i stays within the range of 1-12
+            if i < 1 {
+                i = 12
+            }
+            
+            // Reload the collection view to reflect the updated month
+            collectionView.reloadData()
+        
+            //update the label to the current month and year
+            updateCurrentMonthAndYearLabel()
+
+        }
+    
+    //increases the value of i by 1 and therefore moving to the next month
+    @objc func nextMonthButtonTapped(_ sender: UIButton) {
+            // Increase the value of i by 1
+            i += 1
+            
+            // Ensure i stays within the range of 1-12
+            if i > 12 {
+                i = 1
+            }
+            
+            // Reload the collection view to reflect the updated month
+            collectionView.reloadData()
+        
+            //update the label to the current month and year
+            updateCurrentMonthAndYearLabel()
+
+        }
     
     // Implement UICollectionViewDataSource methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // Return the number of days in the month
-        return 31
+      
+        return monthsDictionary[i]!
+        
+     
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
