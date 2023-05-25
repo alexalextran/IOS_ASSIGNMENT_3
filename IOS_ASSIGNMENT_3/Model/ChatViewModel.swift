ec212617a463045b6b614d8db99b3b26859beb20
Sill Wmith <vietdragon.jv@gmail.com>
2023-05-26 00:40:19 +1000

    updated openai implementations and added more api keys

--- IOS_ASSIGNMENT_3/Model/ChatViewModel.swift
+++ IOS_ASSIGNMENT_3/Model/ChatViewModel.swift
@@ -4,40 +4,35 @@
 //
 //  Created by Sill Wmith on 25/5/2023.
 //
-
 import Foundation
 
-extension OpenAIViewController {
-    class ViewModel {
-        weak var delegate: ViewModelDelegate?
-        var messages: [Message] = [Message(id: UUID(), role: .user, content: "You are a cute cat girl name Nyanpasu and a mental health specialist. You are to give out good advice regarding mental wellbeing and you must provide advice that adheres to the persons individual circumstances. Your advice must help in terms of physical and mental wellbeing. You must also say Meow~! at the start of your sentences and ~nyaa at the end of every sentence before the full stop. Reply in one sentence only", createAt: Date())]
-        var currentInput: String = ""
+extension ChatView {
+    class ViewModel: ObservableObject{
+        @Published var messages: [Message] = [Message(id: UUID(), role: .user, content: "You are a cute catgirl name Nyanpasu and a mental health specialist", createAt: Date())]
+        @Published var currentInput: String = ""
         
-        private let openAIManager = OpenAIManager()
+        private let openAIManager =  OpenAIManager()
         
-        func sendMessage() {
+        
+        func sendMessage(){
             let newMessage = Message(id: UUID(), role: .user, content: currentInput, createAt: Date())
             messages.append(newMessage)
             currentInput = ""
             
             Task {
                 let response = await openAIManager.sendMessage(messages: messages)
-                guard let receivedOpenAIMessage = response?.choices.first?.message else {
+                guard let receivedOpenAIMessage = response?.choices.first?.message else{
                     print("Had no received message")
                     return
                 }
                 let receivedMessage = Message(id: UUID(), role: receivedOpenAIMessage.role, content: receivedOpenAIMessage.content, createAt: Date())
-                await MainActor.run {
+                await MainActor.run{
                     messages.append(receivedMessage)
-                    delegate?.reloadMessages()
                 }
             }
         }
     }
 }
-
-extension OpenAIViewController.ViewModel: ObservableObject {}
-
 
 struct Message: Decodable{
     let id: UUID
@@ -45,3 +40,4 @@ struct Message: Decodable{
     let content: String
     let createAt: Date
 }
+
