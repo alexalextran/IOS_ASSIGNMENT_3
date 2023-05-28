21339d27b8bd1291c7ab7fb048c2ae73179f3796
Alex Tran <85623027+alexalextran@users.noreply.github.com>
2023-05-28 21:21:01 +1000

    Update APIKey.swift

--- IOS_ASSIGNMENT_3/APIKEY/APIKey.swift
+++ IOS_ASSIGNMENT_3/APIKEY/APIKey.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum APIKey {
-    static let openAIAPIKey = ""
+    static let openAIAPIKey = "" //PASTE THE OPENAAI API KEY IN BETWEEN THE " " AS PROVIDED IN THE SUBMISSION'S COMMENTS
     static let openWeatherAPIKey = "a1b773c1afe6bd5321f4256ca8f8a282"
 }
